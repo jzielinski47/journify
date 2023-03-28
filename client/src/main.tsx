1bda46813d667fd276e02d5813ccac4b87723791
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-03-28 23:14:21 +0200

    client-server-ws-connection

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//@ts-ignore
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+export const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
-const http = require("http")
-const express = require("express")
-const path = require("path")
+const http = require("http");
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const WebSocket = require("ws");
-const port = 5000
+const port = 8000;
 
+const webSocketServer = new WebSocket.Server({ server: server });
+webSocketServer.on('connection', (socket, req) => {
+    console.log('new client connected');
+
+    socket.on('close', () => {
+        console.log('client disconnected')
+
+    })
+});
 
 server.listen(port, () => console.log(`app listening on port ${port}`))
