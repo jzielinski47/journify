3d228ca48ab02dbff46068e7e99df8019bc93d99
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-05-17 22:15:19 +0200

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,11 @@
 import { useEffect, useState } from 'react'
 import { client as ws } from '.'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 const App = () => {
   const [isConnected, setIsConnected] = useState(false)
 
@@ -41,13 +46,11 @@ const App = () => {
   return (
     <div className='App'>
 
-      {id}
-
-
-
       <div className='loginForm'>
+        <FontAwesomeIcon icon="fa-solid fa-car-burst" size='5xl' style={{ color: "#ffffff" }} />
         <h2>Sign in</h2>
         <p>Connect your Journify account.<br />Don't have an account? <span className='fake-link'>Sign up!</span></p>
+        <p> {id}</p>
 
         <span className='label'>Username</span>
         <input type='text' onChange={(e) => setLogin(e.target.value)} placeholder='Login' />
