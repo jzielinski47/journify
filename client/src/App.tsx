decf1c78f33278a6e80d0ebc998532b4ae67f024
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-05-18 08:16:18 +0200

    code-refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,22 +2,12 @@
 
 import { useEffect, useState } from 'react'
 import { client as ws } from '.'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
+import LoginPanel from './components/LoginPanel/LoginPanel'
 
 const App = () => {
+
   const [isConnected, setIsConnected] = useState(false)
-  const [isPasswordShown, setIsPasswordShown] = useState(false)
-
   const [errorMessage, setErrorMessage] = useState('')
-
-  const [login, setLogin] = useState('');
-  const [passwd, setPasswd] = useState('');
-
-  const [id, setID] = useState('')
 
   useEffect(() => {
 
@@ -41,33 +31,12 @@ const App = () => {
 
   }, [])
 
-  const sendData = (message: string) => {
-    ws.send(message)
-  }
-
-  const sendLoginData = () => ws.send(`%login&login=${login}&passwd=${passwd}`)
-
-  const sendRegisterData = () => ws.send(`%register&login=${login}&passwd=${passwd}`)
-
+  const sendData = (message: string) => ws.send(message)
 
   return (
     <div className='App'>
 
-      <div className='loginForm'>
-        <FontAwesomeIcon icon="fa-solid fa-car-burst" bounce size='2xl' style={{ color: "#ffffff" }} onClick={() => setIsPasswordShown(!isPasswordShown)} />
-        <h2>Sign in</h2>
-        <p>Connect your Journify account.<br />Don't have an account? <span className='fake-link'>Sign up!</span></p>
-        <p> {id}</p>
-
-        <span className='label'>Username</span>
-        <input type='text' onChange={(e) => setLogin(e.target.value)} placeholder='Login' />
-        <span className='label'>Password</span>
-        <input type={isPasswordShown ? 'text' : 'password'} onChange={(e) => setPasswd(e.target.value)} placeholder='Password' />
-        <p className='error'>{errorMessage}</p>
-        <button className='btn-hmp' onClick={() => sendLoginData()}>Login</button>
-        <button className='btn-mmp' onClick={() => sendRegisterData()}>Sign up</button>
-      </div>
-
+      <LoginPanel ws={ws} errorMessage={errorMessage} />
 
     </div>
   )
---
+++ client/src/components/LoginPanel/LoginPanel.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+const LoginPanel = ({ ws, errorMessage }) => {
+
+    const [isPasswordShown, setIsPasswordShown] = useState(false)
+    
+    const [login, setLogin] = useState('');
+    const [passwd, setPasswd] = useState('');
+    const [id, setID] = useState('')
+
+    const sendLoginData = () => ws.send(`%login&login=${login}&passwd=${passwd}`)
+
+    const sendRegisterData = () => ws.send(`%register&login=${login}&passwd=${passwd}`)
+
+    return (
+        <div className='loginPanel'>
+            <FontAwesomeIcon icon="fa-solid fa-car-burst" bounce size='2xl' style={{ color: "#ffffff" }} onClick={() => setIsPasswordShown(!isPasswordShown)} />
+            <h2>Sign in</h2>
+            <p>Connect your Journify account.<br />Don't have an account? <span className='fake-link'>Sign up!</span></p>
+            <p> {id}</p>
+
+            <span className='label'>Username</span>
+            <input type='text' onChange={(e) => setLogin(e.target.value)} placeholder='Login' />
+            <span className='label'>Password</span>
+            <input type={isPasswordShown ? 'text' : 'password'} onChange={(e) => setPasswd(e.target.value)} placeholder='Password' />
+            <p>Forgot password?</p>
+            <p className='error'>{errorMessage}</p>
+            <button className='btn-hmp' onClick={() => sendLoginData()}>Login</button>
+            <button className='btn-mmp' onClick={() => sendRegisterData()}>Sign up</button>
+        </div>
+    )
+}
+
+export default LoginPanel;
