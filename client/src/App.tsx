00fb3375a6ed949794fbac1f1097d92d4210f585
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-06-02 23:52:38 +0200

    router-routs-update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,19 @@
 import { useEffect, useState } from 'react'
 import { client as ws } from '.'
 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useNavigate } from 'react-router-dom'
+
 import LoginScreen from './screens/LoginScreen/LoginScreen'
+import DashboardScreen from './screens/DashboardScreen/DashboardScreen'
+
 
 const App = () => {
 
   const [isConnected, setIsConnected] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
   const [id, setID] = useState('')
+
+  const navigate = useNavigate();
 
   useEffect(() => {
 
@@ -24,6 +30,7 @@ const App = () => {
 
       if (data.startsWith('%authorized')) {
         setID(data.split('=')[1])
+        navigate("/dashboard");
       } else {
         console.log(data)
         setErrorMessage(data)
@@ -35,9 +42,18 @@ const App = () => {
 
   const sendData = (message: string) => ws.send(message)
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<LoginScreen ws={ws} id={id} errorMessage={errorMessage} />}>
+        <Route index element={<LoginScreen ws={ws} id={id} errorMessage={errorMessage} />} />
+        <Route path='/dashboard' element={<DashboardScreen />} />
+      </Route >
+    )
+  )
+
   return (
     <div className='App'>
-      <LoginScreen ws={ws} errorMessage={errorMessage} id={id} />
+      <RouterProvider router={router} />
     </div>
   )
 }
