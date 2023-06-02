bc35f1770beb8f7fe4c7c69b918ea95b7c1a631e
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-06-02 11:58:52 +0200

    security-addon

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -8,6 +8,11 @@ import LoginPanel from '../components/LoginPanel/LoginPanel'
 // import video4 from '../assets/video4.mp4'
 // import video5 from '../assets/video5.mp4'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 import image from '../assets/benz.jpg'
 
 // const arr = [video1, video2, video3, video4, video5]
@@ -26,6 +31,11 @@ const LoginScreen = ({ ws, errorMessage, id }) => {
             <div className='section'>
                 <h1>Journify</h1>
                 <p>Your personal vechicle assistant<br />{id}</p>
+            </div>
+
+            <div className='security-warning'>
+                {/* <FontAwesomeIcon icon="fa-solid fa-shield-check" style={{ color: "#ffffff", }} /> */}
+                <p>All your data is secured by JAKUB ZIELIŃSKI © 2023</p>
             </div>
 
         </div>
