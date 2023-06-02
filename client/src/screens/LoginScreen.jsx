45e141ef1c94d34679a5acfde428fa8a02833a48
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-06-02 10:33:37 +0200

    optimalization

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 import LoginPanel from '../components/LoginPanel/LoginPanel'
 
-import video1 from '../assets/video1.mp4'
-import video2 from '../assets/video2.mp4'
-import video3 from '../assets/video3.mp4'
-import video4 from '../assets/video4.mp4'
-import video5 from '../assets/video5.mp4'
+// import video1 from '../assets/video1.mp4'
+// import video2 from '../assets/video2.mp4'
+// import video3 from '../assets/video3.mp4'
+// import video4 from '../assets/video4.mp4'
+// import video5 from '../assets/video5.mp4'
 
-const arr = [video1, video2, video3, video4, video5]
+import image from '../assets/merc.jpg'
+
+// const arr = [video1, video2, video3, video4, video5]
 
 import './LoginScreen.css'
 
 const LoginScreen = ({ ws, errorMessage, id }) => {
     return (
         <div className='loginScreen'>
-            <video src={arr[Math.floor(Math.random() * arr.length)]} autoPlay loop muted />
+            {/* <video className="cover-fill" src={arr[Math.floor(Math.random() * arr.length)]} autoPlay loop muted /> */}
+            <img className="cover-fill" src={image} />
 
             <div className='section'>
                 <LoginPanel ws={ws} errorMessage={errorMessage} id={id} />
