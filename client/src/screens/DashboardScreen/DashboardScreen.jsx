487d48643f915c5e975b2b275593804621fcae0d
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-06-04 19:04:17 +0200

    navigation-bug-fix

--- client/src/screens/DashboardScreen/DashboardScreen.jsx
+++ client/src/screens/DashboardScreen/DashboardScreen.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './DashboardScreen.css'
 
+import { useNavigate } from 'react-router-dom'
+
+
 const DashboardScreen = ({ id }) => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => { id ? null : navigate('/', { replace: true }) }, [id])
+
     return (
         <div>{id}</div>
     )
